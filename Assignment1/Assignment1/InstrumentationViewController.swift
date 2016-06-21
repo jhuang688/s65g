ac00068f23172eaf982c8b056eefe927fb148083
Joanne <jhuang688@gmail.com>
2016-06-21 12:14:45 -0400

    moved project folder

---
+++ Assignment1/Assignment1/InstrumentationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Assignment1
+//
+//  Created by Joanne Huang on 21/06/2016.
+//  Copyright © 2016 Joanne Huang. All rights reserved.
+//
+
+import UIKit
+
+class InstrumentationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
