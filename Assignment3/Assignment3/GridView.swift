47ee1599ab3de7013b1e27ae4e54da2f25c44305
Joanne <jhuang688@gmail.com>
2016-07-06 14:21:24 -0400

    first commit - only enum done

---
+++ Assignment3/Assignment3/GridView.swift
@@ -0,0 +1,23 @@
+//
+//  GridView.swift
+//  Assignment3
+//
+//  Created by Joanne Huang on 6/07/2016.
+//  Copyright © 2016 Joanne Huang. All rights reserved.
+//
+
+import Cocoa
+
+class GridView: UIView {
+
+    
+}
+
+
+//Is IBDesignable
+//Has the following IBInspectable Properties:
+//2 Integers: rows and cols (defaulting to 20 and 20, respectively)
+//4 UIColors: livingColor, emptyColor, bornColor, diedColor, gridColor (pick defaults)
+//CGFloat  gridWidth
+//Has the following normal characteristic:
+//a 2-d array of CellState values called grid which is reinitialized to all .Empty values every time that rows or cols changes using the didSet keyword on the rows and cols values.
---
+++ temp.playground/Contents.swift
@@ -0,0 +1,57 @@
+//
+//  CellState.swift
+//  Assignment3
+//
+//  Created by Joanne Huang on 6/07/2016.
+//  Copyright © 2016 Joanne Huang. All rights reserved.
+//
+
+import Foundation
+
+enum CellState : String {
+    case Living = "Living"
+    case Empty = "Empty"
+    case Born = "Born"
+    case Died = "Died"
+    
+    func description() -> String {
+        switch self {
+        case .Living:
+            return Living.rawValue
+        case .Empty:
+            return Empty.rawValue
+        case .Born:
+            return Born.rawValue
+        case .Died:
+            return Died.rawValue
+        }
+    }
+    
+    func allValues() -> [CellState] {
+        //        var allValues: [CellState]
+        //        for state in CellState {
+        //            allValues += CellState.state    // verify if this works - probably doesn't
+        //        }
+        //
+        //        return allValues
+
+        return [Living, Empty, Born, Died]
+    }
+    
+    // LEADING DOT???
+    
+    func toggle(value:CellState) -> CellState {
+        switch value {
+        case .Empty, .Died:
+            return .Living
+        case .Living, .Born:
+            return .Empty
+        }
+    }
+}
+
+CellState.Living.allValues()
+CellState.Living.toggle(.Living)
+CellState.Died.toggle(.Died)
+CellState.Born.description()
+
