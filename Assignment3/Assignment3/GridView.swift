05ac038b6a0edfb164bd218fe386ef363a236c57
Joanne <jhuang688@gmail.com>
2016-07-09 10:57:27 -0400

    broken

--- Assignment3/Assignment3/GridView.swift
+++ Assignment3/Assignment3/GridView.swift
@@ -38,6 +38,11 @@ import UIKit
     
     var grid: [[CellState]] = []
     
+    var gridlinesDrawn = false
+//    var touchAction = false
+//    var touchRow = 0
+//    var touchCol = 0
+    
     override func drawRect(rect: CGRect) {
        // super.drawRect(CGRect)  // not needed
         
@@ -45,47 +50,71 @@ import UIKit
         let cellWidth: CGFloat = self.frame.size.width / CGFloat(cols)
         let cellHeight: CGFloat = self.frame.size.height / CGFloat (rows)  // in case we don't want squares someday
         
-        
-        // draw gridlines
-        let gridLines = UIBezierPath()
-        
-        gridLines.lineWidth = gridWidth
-        
-        for col in 0...cols {
-            gridLines.moveToPoint(CGPoint(x: CGFloat(col) * cellWidth, y: 0))
-            gridLines.addLineToPoint(CGPoint(x: CGFloat(col) * cellWidth, y: CGFloat(rows) * cellHeight))
-        }
-        
-        for row in 0...rows {
-            gridLines.moveToPoint(CGPoint(x: 0, y: CGFloat(row) * cellHeight))
-            gridLines.addLineToPoint(CGPoint(x: CGFloat(cols) * cellWidth, y: CGFloat(row) * cellHeight))
-        }
-        
-        gridColor.setStroke()
-        gridLines.stroke()
- 
-
-        // draw circles in cells
-        for col in 0..<cols {
-            for row in 0..<rows {
-                let aCell = CGRectMake(CGFloat(col)*cellWidth, CGFloat(row)*cellHeight, cellWidth, cellHeight);
-
-                let circle = UIBezierPath(ovalInRect: aCell)
-                var cellColor: UIColor
-                switch (grid[col][row]) {
-                case .Living:
-                    cellColor = livingColor
-                case .Empty:
-                    cellColor = emptyColor
-                case .Born:
-                    cellColor = bornColor
-                case .Died:
-                    cellColor = diedColor
-                }
-                cellColor.setFill()
-                circle.fill()
+        // draw gridlines if not yet drawn
+        if gridlinesDrawn == false {
+            let gridLines = UIBezierPath()
+            
+            gridLines.lineWidth = gridWidth
+            
+            for col in 0...cols {
+                gridLines.moveToPoint(CGPoint(x: CGFloat(col) * cellWidth, y: 0))
+                gridLines.addLineToPoint(CGPoint(x: CGFloat(col) * cellWidth, y: CGFloat(rows) * cellHeight))
             }
+            
+            for row in 0...rows {
+                gridLines.moveToPoint(CGPoint(x: 0, y: CGFloat(row) * cellHeight))
+                gridLines.addLineToPoint(CGPoint(x: CGFloat(cols) * cellWidth, y: CGFloat(row) * cellHeight))
+            }
+            
+            gridColor.setStroke()
+            gridLines.stroke()
+            
+            gridlinesDrawn = true
         }
+        
+//        if touchAction == false {
+            // draw all circles in cells
+            for col in 0..<cols {
+                for row in 0..<rows {
+                    let aCell = CGRectMake(CGFloat(col)*cellWidth, CGFloat(row)*cellHeight, cellWidth, cellHeight);
+                    
+                    let circle = UIBezierPath(ovalInRect: aCell)
+                    var cellColor: UIColor
+                    switch (grid[col][row]) {
+                    case .Living:
+                        cellColor = livingColor
+                    case .Empty:
+                        cellColor = emptyColor
+                    case .Born:
+                        cellColor = bornColor
+                    case .Died:
+                        cellColor = diedColor
+                    }
+                    cellColor.setFill()
+                    circle.fill()
+                }
+            }
+//        }
+        
+        // redraw touched circle only
+//        let aCell = CGRectMake(CGFloat(touchCol)*cellWidth, CGFloat(touchRow)*cellHeight, cellWidth, cellHeight);
+//        
+//        let circle = UIBezierPath(ovalInRect: aCell)
+//        var cellColor: UIColor
+//        switch (grid[touchCol][touchRow]) {
+//        case .Living:
+//            cellColor = livingColor
+//        case .Empty:
+//            cellColor = emptyColor
+//        case .Born:
+//            cellColor = bornColor
+//        case .Died:
+//            cellColor = diedColor
+//        }
+//        cellColor.setFill()
+//        circle.fill()
+//        touchAction = false
+
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -105,6 +134,17 @@ import UIKit
     }
     
     func processTouch(touch: UITouch) {
+//        // calculate size
+//        let cellWidth: CGFloat = self.frame.size.width / CGFloat(cols)
+//        let cellHeight: CGFloat = self.frame.size.height / CGFloat (rows)  // in case we don't want squares someday
+//        
+//        let point: CGPoint = touch.locationInView(self)
+//        touchRow = Int (floor(point.y / cellHeight))
+//        touchCol = Int (floor(point.x / cellWidth))
+//        grid[touchCol][touchRow] = grid[touchCol][touchRow].toggle(grid[touchCol][touchRow])
+//
+//        touchAction = true
+        
         // calculate size
         let cellWidth: CGFloat = self.frame.size.width / CGFloat(cols)
         let cellHeight: CGFloat = self.frame.size.height / CGFloat (rows)  // in case we don't want squares someday
@@ -113,6 +153,7 @@ import UIKit
         let row: Int = Int (floor(point.y / cellHeight))
         let col: Int = Int (floor(point.x / cellWidth))
         grid[col][row] = grid[col][row].toggle(grid[col][row])
+
         self.setNeedsDisplay()
     }
 }
